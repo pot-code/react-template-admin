85ef0d42d4aa7aa8d3d0972847f22fd1dc7ec914
黄冠 <136290354@qq.com>
2023-05-10 16:39:10 +0800

    fix: typo

--- src/router/useSchemaStore.ts
+++ src/router/useSchemaStore.ts
@@ -1,12 +1,12 @@
 import { create } from "zustand"
 import { RouteSchema } from "./type"
 
-interface RouterState {
+interface SchemaState {
   schemas: RouteSchema[]
   setSchemas: (routes: RouteSchema[]) => void
 }
 
-const useSchemaStore = create<RouterState>((set) => ({
+const useSchemaStore = create<SchemaState>((set) => ({
   schemas: [],
   setSchemas: (schemas) => {
     set({ schemas })
