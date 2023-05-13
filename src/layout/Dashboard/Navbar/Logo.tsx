04bceeff9d7f0a928a4d2863110a536de79fe604
pot-code <136290354@qq.com>
2023-05-13 12:04:50 +0800

    feat: add logo

--- src/layout/Dashboard/Navbar/Logo.tsx
+++ src/layout/Dashboard/Navbar/Logo.tsx
@@ -1,3 +1,17 @@
+import { theme } from "antd"
+import logo from "@/assets/logo.png"
+
+const { useToken } = theme
+
 export default function Logo() {
-  return <span>Logo</span>
+  const {
+    token: { controlHeightLG },
+  } = useToken()
+
+  return (
+    <div className="flex gap-4 items-center">
+      <img alt="logo" src={logo} height={controlHeightLG} />
+      <span>React Admin</span>
+    </div>
+  )
 }
