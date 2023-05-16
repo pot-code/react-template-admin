4e631cf001fe85dbb12e00bc4e8e8c6edec69ed0
黄冠 <136290354@qq.com>
2023-05-16 15:14:16 +0800

    chore: update mock data

--- mock/handlers/router.ts
+++ mock/handlers/router.ts
@@ -10,22 +10,27 @@ export const handlers = [
           path: "menu1",
           label: "菜单1",
           order: 3,
+          viewPath: null,
           children: [
             {
               id: "95ea8b637471c22168c26824a4be6e4c877fce59",
               path: "settings",
               label: "子菜单1",
+              order: 1,
+              viewPath: null,
               children: [
                 {
                   id: "fa5a357aa1b93924414262a8954bca42f05a05a4",
                   path: "",
                   label: "列表",
+                  order: 1,
                   hiddenInMenu: true,
                   viewPath: "/menu1/sub1.tsx",
                 },
                 {
                   id: "c69effc08c5d77cc2180ba85c5112d554236675d",
                   path: "edit",
+                  order: 2,
                   label: "编辑",
                   hiddenInMenu: true,
                   viewPath: "/menu1/sub2.tsx",
@@ -34,6 +39,7 @@ export const handlers = [
             },
             {
               id: "a5e078d4fc35e7edf9025cb74c10e985245086e9",
+              order: 2,
               path: "appearance",
               label: "子菜单2",
               viewPath: "/menu1/sub2.tsx",
