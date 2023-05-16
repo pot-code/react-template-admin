524bb2bae8880552ac416d6ef09bdc4dc592a8f6
pot-code <136290354@qq.com>
2023-05-16 18:43:43 +0800

    style: system menu style

--- src/views/system/menu.tsx
+++ src/views/system/menu.tsx
@@ -1,16 +1,21 @@
-import { Card, Col, Empty, Row } from "antd"
+import { Card, Col, Empty, Row, theme } from "antd"
 import useMenu from "@/features/system/menu/useMenu"
 import ContentLoading from "@/components/ContentLoading"
 import SchemaForm from "@/features/system/menu/SchemaForm"
 import TreeView from "@/features/system/menu/TreeView"
 
+const { useToken } = theme
+
 export function Menu() {
   const { isLoading, treeNodes, selectedRoute, onSelect } = useMenu()
+  const {
+    token: { padding },
+  } = useToken()
 
   if (isLoading) return <ContentLoading />
 
   return (
-    <Row gutter={8}>
+    <Row className="h-full" gutter={padding}>
       <Col span={6}>
         <Card className="h-full" title="菜单树" type="inner">
           <TreeView showLine treeData={treeNodes} onSelect={onSelect} />
@@ -18,13 +23,7 @@ export function Menu() {
       </Col>
       <Col span={18}>
         <Card title="设置" type="inner">
-          {selectedRoute ? (
-            <SchemaForm data={selectedRoute} />
-          ) : (
-            <div className="center-child">
-              <Empty description="请选择菜单" />
-            </div>
-          )}
+          {selectedRoute ? <SchemaForm data={selectedRoute} /> : <Empty description="请选择菜单" />}
         </Card>
       </Col>
     </Row>
