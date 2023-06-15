101d42f600dd7a30dc07f6204fc7eff25b8d2b9e
pot-code <136290354@qq.com>
2023-06-15 16:40:20 +0800

    feat(system/menu): reduce request times

--- src/features/system/menu/use-fetch-menu.ts
+++ src/features/system/menu/use-fetch-menu.ts
@@ -1,11 +1,14 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query"
 import { menuApi } from "./api"
+import { Time } from "@/utils/duration"
 
 const queryKey = ["system", "menu"]
 
 export default function useFetchMenu() {
   const qc = useQueryClient()
-  const { data, isSuccess, isLoading } = useQuery(queryKey, () => menuApi.list().then((res) => res.data))
+  const { data, isSuccess, isLoading } = useQuery(queryKey, () => menuApi.list().then((res) => res.data), {
+    staleTime: Time.Minutes * 10,
+  })
 
   function invalidateCache() {
     qc.invalidateQueries(queryKey)
---
+++ src/utils/duration.ts
@@ -0,0 +1,6 @@
+export enum Time {
+  Milliseconds = 1,
+  Seconds = 1000,
+  Minutes = 60 * Seconds,
+  Hours = 60 * Minutes,
+}
